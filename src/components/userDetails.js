5555c147e4b0d2f6ce885d84a40cfe8df608b529
Deji Omoloja <dejiomoloja@Dejis-MacBook-Air.local>
2022-07-15 11:23:20 +0100

    setup responsiveness with media queries

--- src/components/userDetails.js
+++ src/components/userDetails.js
@@ -5,15 +5,15 @@ const UserDetails = ({data, setData}) => {
     return (
         <div className="details-container">
             <div className="container">
-                <FaEnvelope style={{width: "15px", position: "absolute", top: "15px", left: "10px"}}/>
+                <FaEnvelope className='icons'/>
                 <input type="email" name="" value={data.email} placeholder="email" className="input" required onChange={(e)=>setData({...data, email: e.target.value})}/>
             </div>
             <div className="container">
-                <FaUserEdit style={{width: "15px", position: "absolute", top: "15px", left: "10px"}}/>
+                <FaUserEdit className='icons'/>
                 <input type="text" name="" value={data.firstname} placeholder="firstname" className="input" onChange={(e)=>setData({...data, firstname: e.target.value})}/>
             </div>
             <div className="container">
-                <FaUserEdit style={{width: "15px", position: "absolute", top: "15px", left: "10px"}}/>
+                <FaUserEdit className='icons'/>
                 <input type="text" name="" value={data.lastname} placeholder="lastname" className="input" onChange={(e)=>setData({...data, lastname: e.target.value})}/>
             </div>
         </div>
--- src/components/userPassword.js
+++ src/components/userPassword.js
@@ -4,11 +4,11 @@ const UserPassword = ({data, setData}) => {
     return(
         <div className="password-container">
             <div className="container">
-                <FaFingerprint style={{width: "15px", position: "absolute", top: "15px", left: "10px"}}/>
+                <FaFingerprint className='icons'/>
                 <input type="password" name="" value={data.password} placeholder="password" className="input" onChange={(e)=>setData({...data, password: e.target.value})}/>
             </div>
             <div className="container">
-                <FaFingerprint style={{width: "15px", position: "absolute", top: "15px", left: "10px"}}/>
+                <FaFingerprint className='icons'/>
                 <input type="password" name="" value={data.confirmpassword} placeholder="confirm your password" className="input" onChange={(e)=>setData({...data, confirmpassword: e.target.value})}/>
             </div>
         </div>
